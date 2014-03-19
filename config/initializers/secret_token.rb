61cf37a79d6f7462587285d5271672c8f02e8a50
root <root@foxy.(none)>
2014-03-19 16:15:52 +0000

    Creat new Rails app with Postgres Server
    used the command
    
    rails new TestRailsOnDigitalOcean --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestRailsOnDigitalOcean::Application.config.secret_key_base = 'fcc8c1a9592bb4499951aa4b07ee0671676eb6f87295ad2c5ce1befc0eaa57b7c212947156f7df3175a813fa2af649d2b9975e57022ce2ae1517259d88d501b4'
