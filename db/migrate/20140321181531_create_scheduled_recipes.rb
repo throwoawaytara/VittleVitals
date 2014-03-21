2051a20f8c092fa5268103907bb6025e8105fc9c
Apprentice <calvache.diana@gmail.com>
2014-03-21 13:34:13 -0500

    Fixed typos in the create_scheduled_recipes migration

--- db/migrate/20140321181531_create_scheduled_recipes.rb
+++ db/migrate/20140321181531_create_scheduled_recipes.rb
@@ -2,8 +2,8 @@ class CreateScheduledRecipes < ActiveRecord::Migration
   def change
     create_table :scheduled_recipes do |t|
       t.date
-      t.recipe_id
-      t.user_id
+      t.integer :recipe_id
+      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321015557) do
+ActiveRecord::Schema.define(version: 20140321181531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,13 @@ ActiveRecord::Schema.define(version: 20140321015557) do
     t.integer  "ingredient_id"
     t.integer  "recipe_id"
     t.integer  "ingredient_quantity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "scheduled_recipes", force: true do |t|
+    t.integer  "recipe_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
