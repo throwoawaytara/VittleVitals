2931d319acf204eb2d5e8357295597ed37eed878
Apprentice <calvache.diana@gmail.com>
2014-03-21 17:59:31 -0500

    Views for recipes completed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,10 @@
 class RecipesController < ApplicationController
 
   def index
-    if user_signed_in?
-      @recipes = User.find(params[:user_id]).recipes
+    if user_signed_in? && !params[:search]
+      return @recipes = User.find(params[:user_id]).recipes
     end
-    elsif params[:search]
+    if params[:search]
       @recipes = Recipe.search(params[:search], params[:method])
     else
       @recipes =  Recipe.all
