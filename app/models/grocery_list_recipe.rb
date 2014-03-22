182a2e5d01482758236958fe29faf3366903aaee
Tara Roys <tlroys@gmail.com>
2014-03-22 12:38:33 -0500

    Grocery list example is seeded
    User 1 now has a grocery list. It looks nice in the view.
    
    Bugfix: grocery list model was trying to validate a field
    that didn't exist.

--- app/models/grocery_list_recipe.rb
+++ app/models/grocery_list_recipe.rb
@@ -2,6 +2,6 @@ class GroceryListRecipe < ActiveRecord::Base
   belongs_to :recipe
   belongs_to :grocery_list
 
-  validates_presence_of :grocery_id, :recipe_id
+  validates_presence_of :grocery_list_id, :recipe_id
 end
 
