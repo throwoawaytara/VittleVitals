9c0a5d05738aa11a53b05df76e0412cf5d290624
calvached <calvache.diana@gmail.com>
2014-03-23 17:07:44 -0500

    Added form the recipe partial

--- app/controllers/grocerylists_controller.rb
+++ app/controllers/grocerylists_controller.rb
@@ -3,8 +3,10 @@ class GrocerylistsController < ApplicationController
     if current_user
       @grocery_list = current_user.grocery_lists.first
       @list_items = []
-      @grocery_list.recipes.map do |recipe|
-        @list_items << recipe.ingredients.to_a
+      if @grocery_list
+        @grocery_list.recipes.map do |recipe|
+          @list_items << recipe.ingredients.to_a
+        end
       end
       @list_items.flatten!
     else
