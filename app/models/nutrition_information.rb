7e6d4bac29432415a2bae6242d196940fb0e5401
Cristin O'Connor <cristinjaneo@gmail.com>
2014-03-23 13:00:27 -0500

    pre switching ruby version

--- app/models/nutrition_information.rb
+++ app/models/nutrition_information.rb
@@ -2,10 +2,10 @@ class NutritionInformation < ActiveRecord::Base
   
   belongs_to :ingredient
 
-  validates :item_name, presence: true
-  validates :ingredient_id, presence: true
-  validates :item_description, presence: true
-  validates :nf_serving_size_qty, presence: true
+  # validates :item_name, presence: true
+  # validates :ingredient_id, presence: true
+  # validates :item_description, presence: true
+  # validates :nf_serving_size_qty, presence: true
 
   validates_presence_of :ingredient_id
 
