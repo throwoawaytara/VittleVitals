0d19d9bd133f1860802aba3f8573eefd49b9a5c2
Cristin O'Connor <cristinjaneo@gmail.com>
2014-03-24 12:00:43 -0500

    completed method to import data from a csv in rails - rails has some spec ial things i had to hack around

--- db/seeds.rb
+++ db/seeds.rb
@@ -167,8 +167,61 @@ ScheduledRecipe.create(day: 'Friday', recipe_id: 1, user_id: 1)
 
 require 'csv'
 file = "#{Rails.root}/db/usda_test.csv"
-CSV.foreach(file, :headers => true, :encoding => 'ISO-8859-1:UTF-8') do |row|
-    NutritionInformation.create(row)
+CSV.foreach(file, headers: true) do |row|
+    NutritionInformation.create(
+db_no: row[0],
+name: row[1],
+water: row[2],
+calories: row[3],
+protein: row[4],
+total_fat: row[5],
+ash: row[6],
+carbs: row[7],
+fiber: row[8],
+sugars: row[9],
+calcium: row[10],
+iron: row[11],
+magnesium: row[12],
+phosphorus: row[13],
+potassium: row[14],
+sodium: row[15],
+zinc: row[16],
+copper: row[17],
+manganese: row[18],
+selenium: row[19],
+vit_c: row[20],
+thiamin: row[21],
+riboflavin: row[22],
+niacin: row[23],
+panto_acid: row[24],
+vit_b6: row[25],
+folate_total: row[26],
+folic_acid: row[27],
+food_folate: row[28],
+folate_dfe: row[29],
+choline_tot: row[30],
+vit_b12: row[31],
+vit_a_iu: row[32],
+vit_a_rae: row[33],
+retinol: row[34],
+alpha_carot: row[35],
+beta_carot: row[36],
+beta_crypt: row[37],
+lycopene: row[38],
+lut_zeaz: row[39],
+vit_e: row[40],
+vit_d_mcg: row[41],
+vivit_d_iu: row[42],
+vit_k: row[43],
+fa_sat: row[44],
+fa_mono: row[45],
+fa_poly: row[46],
+cholestrl: row[47],
+gram_weight_a: row[48],
+gram_weight_unit_a: row[49],
+gram_weight_b: row[50],
+gram_weight_unit_b: row[51],
+refuse_pc: row[52])
 end
 
 # # Create Nutrition Information
