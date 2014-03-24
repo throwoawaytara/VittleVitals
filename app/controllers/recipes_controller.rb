e4cd3f6a5fbe7668e5c763e083e19ea867564bf9
calvached <calvache.diana@gmail.com>
2014-03-24 16:26:46 -0500

    implementing search with Yummly

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,5 @@
 class RecipesController < ApplicationController
   def index
-    @results = Yummly.search('lasagna')
     if params[:search]
       @recipes = Recipe.search(params[:search], params[:search_type])
     else
@@ -12,11 +11,25 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
-    @ingredients_qty_units = @recipe.collect_ingredients_quantities_units
-    
-
     @nutrition_info = @recipe.nutrition_calc
     @loaded_ingredients = "if not all ingredients were returned with nutrition data, list the ones that weren't here"
   end
   # If no nutrition data is avaiable then do not load the nutrition section in the view
+
+  def cache_database(query)
+    recipe = get_recipe_from_yummly(query)
+    recipe_id = recipe.matches.first["id"]
+    ingredients_arr = recipe.matches.first["ingredients"]
+    recipe_details = get_details_from_yummly(recipe_id)
+    serving_size = recipe_details.json['numberOfServings']
+    # Recipe.create
+  end
+
+  def get_recipe_from_yummly(query)
+    Yummly.search(query, {max: 1})
+  end
+
+  def get_details_from_yummly(recipe_id)
+    Yummly.find(recipe_id)
+  end
 end
--- app/controllers/scheduled_recipes_controller.rb
+++ app/controllers/scheduled_recipes_controller.rb
@@ -31,8 +31,8 @@ class ScheduledRecipesController < ApplicationController
   def meals_scheduled_for(day)
      @day_recipes = []
       current_user.scheduled_recipes.where(day: day).each do |schedule|
-        schedule.recipe?
-        # @day_recipes << Recipe.where(id: schedule.recipe_id)
+        # schedule.recipe?
+        @day_recipes << Recipe.where(id: schedule.recipe_id)
       end
       @day_recipes
   end
