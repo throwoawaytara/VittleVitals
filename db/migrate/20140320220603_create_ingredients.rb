f78e605e9a644cfce2e5228ce0064354f763cccc
Cristin O'Connor <cristinjaneo@gmail.com>
2014-03-24 12:06:38 -0500

    added brand field back with a default so it didn't break our app

--- db/migrate/20140320220603_create_ingredients.rb
+++ db/migrate/20140320220603_create_ingredients.rb
@@ -2,7 +2,8 @@ class CreateIngredients < ActiveRecord::Migration
   def change
     create_table :ingredients do |t|
       t.string :name
-      
+      t.string :brand, default: "no brand"
+
       t.timestamps
     end
   end
