1717bd28cb735acbfc18e23b6cbb4d337429720e
Tara Roys <tlroys@gmail.com>
2014-03-25 17:13:58 -0500

    Revert "my recipes fav and add buttons"
    This reverts commit a239926ffea771ec26b98be9c20a90b9c03f5748.

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -2,7 +2,7 @@ class UserRecipesController < ApplicationController
   def index
     if current_user
       user = User.find(params[:user_id])
-      @user_recipes = user.recipes
+      @recipes = user.recipes
     else
       redirect_to '/'
     end
