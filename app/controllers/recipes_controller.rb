bd0a64909723970c6225648302afd10735456b13
Cristin O'Connor <cristinjaneo@gmail.com>
2014-03-25 12:02:03 -0500

    added ingredients back to single recipe view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,6 +11,8 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+    @ingredients = @recipe.ingredients
+
     # @nutrition_info = @recipe.nutrition_calc
 
   end
