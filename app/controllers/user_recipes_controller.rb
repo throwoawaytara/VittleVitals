a239926ffea771ec26b98be9c20a90b9c03f5748
Cristin O'Connor <cristinjaneo@gmail.com>
2014-03-25 16:57:44 -0500

    my recipes fav and add buttons

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -2,7 +2,7 @@ class UserRecipesController < ApplicationController
   def index
     if current_user
       user = User.find(params[:user_id])
-      @recipes = user.recipes
+      @user_recipes = user.recipes
     else
       redirect_to '/'
     end
