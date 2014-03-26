e5cec9317064920308850259154463642a8fdc3f
Cristin O'Connor <cristinjaneo@gmail.com>
2014-03-26 10:15:44 -0500

    field added

--- db/migrate/20140324135426_create_grocery_list_ingredients.rb
+++ db/migrate/20140324135426_create_grocery_list_ingredients.rb
@@ -3,6 +3,7 @@ class CreateGroceryListIngredients < ActiveRecord::Migration
     create_table :grocery_list_ingredients do |t|
       t.integer :ingredient_id
       t.integer :grocery_list_id
+      t.integer :position
     end
   end
 end
